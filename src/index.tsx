eed0fc01bf7b03430a2ce4e9a5c6fbae39f70941
hjw4595 <hongjung73@gmail.com>
2022-07-06 02:54:10 +0900

    fix / react 18 waring fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
+
+import App from "./App";
+
+import "./index.css";
 // import { Provider } from "react-redux";
 // import rootReducer from "store/reducers";
 // import { createStore } from "redux";
 
 // const store = createStore(rootReducer);
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <RecoilRoot>
       <App />
     </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

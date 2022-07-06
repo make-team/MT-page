ef671671a660ef24e1e84aaf6dfbadd4dabb70fd
hjw4595 <hongjung73@gmail.com>
2022-07-06 14:07:08 +0900

    add / global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import Root from "./route/Root";
 
+import { createGlobalStyle } from "styled-components";
+
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <Root />
-    </div>
+    </>
   );
 }
-
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: "Helvetica", "Arial", sans-serif;
+    line-height: 1.5;
+    user-select:none
+  }
+
+  h2, p {
+    margin: 0;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  p {
+    font-size: 1rem; 
+  }
+`;
